9ced99d15841eaf99d91a54782c5f74e603c3c69
yourUncleMike <m.d.clay@gmail.com>
2017-04-30 15:36:37 -0700

    adding PoC.html and PoC.user.js

---
+++ Greasemonkey/PoC.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name        proof-of-concept
+// @namespace   yourUncleMike
+// @include     file:///home/md_clay/yourUncleMike/Greasemonkey/*.html
+// @version     2017.04.30.1448
+// ==/UserScript==
+var version  = '2017.04.30.1448'
+
+//////////////////////////////////////////////////
+//                                              //
+// IF YOU ARE SEEING THIS MESSAGE, YOU NEED TO: //
+// 1. Install Greasemonkey 3.11 or higher; and  //
+// 2. Enable Greasemonkey                       //
+//                                              //
+//////////////////////////////////////////////////
+
+readyState = document.readyState;
+if (readyState) {
+  alert("document.readyState: " + readyState);
+}
+
+documentReadyState_field = document.getElementById("document.readyState");
+if (documentReadyState_field) {
+  alert("document.ReadyState: " + readyState);
+  if (readyState) {
+    documentReadyState_field.value = readyState;
+  } else {
+    documentReadyState_field.value = typeof readyState;
+  }
+}
+
+greasemonkeyVersion_field = document.getElementById("GM_info.version");
+if (greasemonkeyVersion_field) {
+  greasemonkeyVersion_field.value = GM_info.version;
+}
+
+userscriptVersion_field = document.getElementById("GM_info.script.version");
+if (userscriptVersion_field) {
+  userscriptVersion_field.value = GM_info.script.version;
+}
+
+listValues_field = document.getElementById("GM_listValues");
+if (listValues_field) {
+  listValues_field.value = typeof GM_listValues;
+}
+
+xmlhttpRequest_field = document.getElementById("GM_xmlhttpRequest");
+if (xmlhttpRequest_field) {
+  xmlhttpRequest_field.value = typeof GM_xmlhttpRequest;
+}
