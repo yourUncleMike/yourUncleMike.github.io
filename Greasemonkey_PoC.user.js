7b438ccf418106e07c661cba79578d12e9f6c176
yourUncleMike <m.d.clay@gmail.com>
2017-04-30 22:08:23 -0700

    Tried iframe. No joy...

--- Greasemonkey_PoC.user.js
+++ Greasemonkey_PoC.user.js
@@ -3,9 +3,9 @@
 // @namespace   yourUncleMike
 // @include     file:///home/md_clay/yourUncleMike.github.io/*.html
 // @include     https://yourunclemike.github.io/*.html
-// @version     2017.04.30.1653
+// @version     2017.04.30.2207
 // ==/UserScript==
-var version  = '2017.04.30.1653'
+var version  = '2017.04.30.2207'
 
 //////////////////////////////////////////////////
 //                                              //
