54df73b6a9eaab01934c62723429ccaabb7d9dc7
yourUncleMike <m.d.clay@gmail.com>
2017-12-31 11:21:47 -0800

    buttons.html - to show off [-/+] buttons and the new [less/more/...] buttons in js/button.js

--- js/button.js
+++ js/button.js
@@ -1,12 +1,36 @@
 function button_reveal(button) {
-  var span = button.nextElementSibling;
-  var div = span.nextElementSibling;
-  div.hidden = button.textContent[0] == "+";
-  span.style.fontWeight = div.hidden ? "bold" : "normal";
+  if (button.textContent[0] == "+") {
+    var next_elem_1 = button.nextElementSibling;
+    var next_elem_2 = next_elem_1.nextElementSibling;
+    next_elem_2.hidden = true;
+    next_elem_1.style.fontWeight = "bold";
+  } else if (button.textContent[0] == "-") {
+    var next_elem_1 = button.nextElementSibling;
+    var next_elem_2 = next_elem_1.nextElementSibling;
+    next_elem_2.hidden = false;
+    next_elem_1.style.fontWeight = "normal";
+  } else if (button.textContent == "less") {
+    var prev_elem_1 = button.previousElementSibling;
+    prev_elem_1.hidden = false;
+  } else if (button.textContent == "more" || button.textContent == "..." ) {
+    var prev_elem_1 = button.previousElementSibling;
+    prev_elem_1.hidden = true;
+  }
 }
 
 function button_toggle(button) {
-  button.textContent = (button.textContent[0] == "-" ? "+" : "-") + button.textContent.slice(1);
+  if      (button.textContent[0] == "+") {
+           button.textContent = "-" + button.textContent.slice(1);
+  } else if (button.textContent[0] == "-") {
+           button.textContent = "+" + button.textContent.slice(1);
+  } else if (button.textContent == "less") {
+           button.textContent = "more";
+  } else if (button.textContent == "more") {
+           button.textContent = "less";
+  } else if (button.textContent == "...") {
+           button.textContent = "less";
+           button.hidden = true;
+  };
   button_reveal(button);
 }
 
