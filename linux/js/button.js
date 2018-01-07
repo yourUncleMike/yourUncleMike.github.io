086387526af716dfefc192efafcd303fe3b76006
yourUncleMike <m.d.clay@gmail.com>
2018-01-07 01:28:46 -0800

    Linux Mint 18.3 - first draft

---
+++ linux/js/button.js
@@ -0,0 +1,41 @@
+function button_reveal(button) {
+  if (button.textContent[0] == "+") {
+    var next_elem_1 = button.parentNode.nextElementSibling.children[0];
+    var next_elem_2 = next_elem_1.nextElementSibling;
+    next_elem_2.hidden = true;
+    next_elem_1.style.fontWeight = "bold";
+  } else if (button.textContent[0] == "-") {
+    var next_elem_1 = button.parentNode.nextElementSibling.children[0];
+    var next_elem_2 = next_elem_1.nextElementSibling;
+    next_elem_2.hidden = false;
+    next_elem_1.style.fontWeight = "normal";
+  } else if (button.textContent == "less") {
+    var prev_elem_1 = button.previousElementSibling;
+    prev_elem_1.hidden = false;
+  } else if (button.textContent == "more" || button.textContent == "..." ) {
+    var prev_elem_1 = button.previousElementSibling;
+    prev_elem_1.hidden = true;
+  }
+}
+
+function button_toggle(button) {
+  if      (button.textContent[0] == "+") {
+           button.textContent = "-" + button.textContent.slice(1);
+  } else if (button.textContent[0] == "-") {
+           button.textContent = "+" + button.textContent.slice(1);
+  } else if (button.textContent == "less") {
+           button.textContent = "more";
+  } else if (button.textContent == "more") {
+           button.textContent = "less";
+  } else if (button.textContent == "...") {
+           button.textContent = "less";
+           button.hidden = true;
+  };
+  button_reveal(button);
+}
+
+function buttons_init() {
+  buttons = document.getElementsByTagName("button");
+  for (var i = 0; i < buttons.length; i++)
+    button_reveal(buttons[i])
+}
